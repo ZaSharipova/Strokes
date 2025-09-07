5df4a959f6ccca99b5eb69ff2c68d72a430aff40
Зарина Шарипова <Sh.zarina.ild@gmail.com>
2025-09-07 17:39:48 +0300

    Добавила новые функции 2.0

--- StrokeFunctions.cpp
+++ StrokeFunctions.cpp
@@ -190,49 +190,6 @@ int Mgetline(FILE *file, char *str, int num) {
     return i;
 }
 
-// size_t my_getline(char **lineptr, size_t *n, FILE *stream) {
-//     if (lineptr == NULL || n == NULL || stream == NULL) {
-//         return -1;
-//     }
-
-//     if (*lineptr == NULL || *n == 0) {
-//         *n = 128; 
-//         *lineptr = (char *) malloc(*n);
-//         if (*lineptr == NULL) {
-//             return -1;
-//         }
-//     }
-
-//     size_t pos = 0;
-//     int c;
-
-//     while ((c = fgetc(stream)) != EOF) {
-
-//         if (pos + 1 >= *n) {
-//             size_t new_size = *n * 2;
-//             char *new_ptr = (char *) realloc(*lineptr, new_size);
-//             if (new_ptr == NULL) {
-//                 return -1;
-//             }
-//             *lineptr = new_ptr;
-//             *n = new_size;
-//         }
-
-//         (*lineptr)[pos++] = (char)c;
-
-//         if (c == '\n') {
-//             break;
-//         }
-//     }
-
-//     if (pos == 0 && c == EOF) {
-//         return -1;
-//     }
-
-//     (*lineptr)[pos] = '\0';
-//     return (size_t)pos;
-// }
-
 long Matoi(const char *str) {
     assert(str != NULL);
     if (str == NULL) {
